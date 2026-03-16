34a2caf1bc59007c0993539e6d3239ef6d531705
S.Matuzov <ven0m2031@mail.ru>
2026-03-16 23:32:15 +0400

    camera tracker added

--- Assets/Scripts/Bird/Bird.cs
+++ Assets/Scripts/Bird/Bird.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+public class Bird : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
